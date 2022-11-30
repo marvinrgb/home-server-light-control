f072cd58a6c8ab01ed56bf04d7ccfee704d33d3e
marvinrgb <82820752+marvinrgb@users.noreply.github.com>
2022-11-30 11:42:22 +0100

    card rotate works hehe

--- rotatetomouse/index.js
+++ rotatetomouse/index.js
@@ -1,6 +1,14 @@
 let card = document.getElementById('card');
 
 card.addEventListener('mousemove', (event) => {
-  console.log("X: " + (event.clientX / 5) + "%")
-  console.log("Y: " + (event.clientY / 5) + "%")
+  console.log("X: " + ((event.clientX - 250) / 25) + " / 10")
+  console.log("Y: " + ((event.clientY - 250) / 25) + " / 10")
+
+  let x = 0 - ((event.clientY - 250) / 10)
+  let y = ((event.clientX - 250) / 10)
+
+
+  event.target.style.transform = `perspective(800px) rotateX(${x}deg) rotatey(${y}deg)`
+
+
 })
