d3ea7cc7927895d87e97f4b8ec1edbd429ed5f41
marvinrgb <marvinwolfart2@gmail.com>
2022-11-30 17:45:12 +0100

    fix

--- rotatetomouse/index.js
+++ rotatetomouse/index.js
@@ -1,14 +1,7 @@
 let card = document.getElementById('card');
 
 card.addEventListener('mousemove', (event) => {
-  console.log("X: " + ((event.clientX - 250) / 25) + " / 10")
-  console.log("Y: " + ((event.clientY - 250) / 25) + " / 10")
-
   let x = 0 - ((event.clientY - 250) / 10)
   let y = ((event.clientX - 250) / 10)
-
-
   event.target.style.transform = `perspective(800px) rotateX(${x}deg) rotatey(${y}deg)`
-
-
 })
